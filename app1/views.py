f626f2ceca5aa5a60c593dbebba77232546f9a05
田松实 <kireal@liive.cn>
2022-11-08 23:41:56 +0800

    这个改了去重

--- app1/views.py
+++ app1/views.py
@@ -24,9 +24,10 @@ def consult(request):
     vbxhm = request.POST.get("bxhm")  # 输入的手册号
     vxm = request.POST.get("xm")  # 输入的姓名
     vsfhm = request.POST.get("sfhm")  # 输入的身份证号码
-    xzbxhm = []  # 所有表中筛选后的保险手册号
-    xzsfhm = []  # 所有表中筛选后的身份证号
-    dtsj_old_bxhm = []  # dtsj_old筛选结果中的保险手册号
+    xs_name = []
+    xs_bxhm = []
+    xs_sfhm = []
+    # dtsj_old_bxhm = []  # dtsj_old筛选结果中的保险手册号
     dtsj_old_sxjg = None  # dtsj_old数据表中的筛选结果
     jbxx_xin_bxhm = []  # jbxx_xin筛选结果中的保险手册号
     jbxx_xin_sfhm = []  # jbxx_xin筛选结果中的身份证号
@@ -53,9 +54,9 @@ def consult(request):
 
     if vsfhm is not None:
         if len(str.strip(vsfhm)) > 0:
+            dtsj_old_sxjg = DtsjOld.objects.filter(bxhm=vsfhm)
             jbxx_xin_sxjg = JbxxXin.objects.filter(sfhm=vsfhm)
             jbxx_zl_sxjg = JbxxZl.objects.filter(sfhm_zl=vsfhm)
-
     # 判断jbxx_dt数据表是否查询到结果
     if jbxx_xin_sxjg is None:
         return render(request, "consult.html")
@@ -63,48 +64,56 @@ def consult(request):
     for item_jbxx_xin in jbxx_xin_sxjg:
         item_grzh_xin = GrzhXin.objects.filter(sfhm_xin=item_jbxx_xin.sfhm).distinct()
         grzh_xin_sxjg.append(item_grzh_xin)
-        for i in item_jbxx_xin.name: # 去重
-            if i not in jbxx_xin_name:
-                jbxx_xin_name.append(item_jbxx_xin.name)  # 将所有jbxx_xin_sxjg中的保险手册号传入jbxx_xin_bxhm中
-        for i in item_jbxx_xin.bxhm: # 去重
-            if i not in jbxx_xin_bxhm:
-                jbxx_xin_bxhm.append(item_jbxx_xin.bxhm)  # 将所有jbxx_xin_sxjg中的保险手册号传入jbxx_xin_bxhm中
-        for i in item_jbxx_xin.sfhm: # 去重
-            if i not in jbxx_xin_sfhm:
-                jbxx_xin_sfhm.append(item_jbxx_xin.sfhm)  # 将所有jbxx_xin_sxjg中的保身份证号传入jbxx_xin_sfhm中
+        for i in jbxx_xin_sxjg:  # 去重
+            if i.name not in jbxx_xin_name:
+                jbxx_xin_name.append(i.name)  # 将所有jbxx_xin_sxjg中的姓名传入jbxx_xin_name中
+                xs_name.append(i.name)
+        for i in jbxx_xin_sxjg:  # 去重
+            if i.bxhm not in jbxx_xin_bxhm:
+                jbxx_xin_bxhm.append(i.bxhm)  # 将所有jbxx_xin_sxjg中的保险手册号传入jbxx_xin_bxhm中
+                xs_bxhm.append(i.bxhm)
+        for i in jbxx_xin_sxjg:  # 去重
+            if i.sfhm not in jbxx_xin_sfhm:
+                jbxx_xin_sfhm.append(i.sfhm)  # 将所有jbxx_xin_sxjg中的保身份证号传入jbxx_xin_sfhm中
+                xs_sfhm.append(i.sfhm)
     # 判断jjbxx_zl数据表是否查询到结果
     if jbxx_zl_sxjg is None:
         return render(request, "consult.html")
-
     for item_jbxx_zl in jbxx_zl_sxjg:
         item_grzh_zl = GrzhZl.objects.filter(sfhm_grzl=item_jbxx_zl.sfhm_zl).distinct()
         grzh_zl_sxjg.append(item_grzh_zl)
-        for i in item_jbxx_zl.name_zl: # 去重
-            if i not in jbxx_zl_bxhm:
-                jbxx_zl_bxhm.append(item_jbxx_zl.bxhm_zl)  # 将所有jbxx_zl_sxjg中的保险手册号传入jbxx_zl_bxhm中
-        for i in item_jbxx_zl.name_zl: # 去重
-            if i not in jbxx_zl_name:
-                jbxx_zl_sfhm.append(item_jbxx_zl.sfhm_zl)  # 将所有jbxx_zl_sxjg中的身份证号传入jbxx_zl_sfhm中
-        for i in item_jbxx_zl.name_zl: # 去重
-            if i not in jbxx_zl_name:
-                jbxx_zl_name.append(item_jbxx_zl.name_zl)  # 将所有jbxx_zl_sxjg中的姓名传入jbxx_zl_name中
-
+    for i in jbxx_zl_sxjg:  # 去重
+        if i.bxhm_zl not in xs_bxhm:
+            xs_bxhm.append(i.bxhm_zl)
+        if i.bxhm_zl not in jbxx_zl_bxhm:
+            jbxx_zl_bxhm.append(i.bxhm_zl)  # 将所有jbxx_zl_sxjg中的保险手册号传入jbxx_zl_bxhm中
+    for i in jbxx_zl_sxjg:  # 去重
+        if i.sfhm_zl not in xs_sfhm:
+            xs_sfhm.append(i.sfhm_zl)
+        if i.sfhm_zl not in jbxx_zl_sfhm:
+            jbxx_zl_sfhm.append(i.sfhm_zl)  # 将所有jbxx_zl_sxjg中的身份证号传入jbxx_zl_sfhm中
+    for i in jbxx_zl_sxjg:  # 去重
+        if i.name_zl not in xs_name:
+            xs_name.append(i.name_zl)
+        if i.name_zl not in jbxx_zl_name:
+            jbxx_zl_name.append(i.name_zl)  # 将所有jbxx_zl_sxjg中的姓名传入jbxx_zl_name中
+    print(jbxx_zl_name)
     if dtsj_old_sxjg is None:
-        dtsj_old_sxjg = DtsjOld.objects.filter(name=jbxx_zl_name[0])
+        dtsj_old_sxjg = DtsjOld.objects.filter(name=xs_name)
     # 将所有dtsj_old_sxjg中的保险手册号传入dtsj_old_bxhm中
-    for item_old_bxhm in dtsj_old_sxjg:
-        for i in item_old_bxhm: # 去重
-            if i not in dtsj_old_bxhm:
-                dtsj_old_bxhm.append(item_old_bxhm.bxhm)
-
+    # for item_old_bxhm in dtsj_old_sxjg:
+    #     for i in item_old_bxhm:  # 去重
+    #         if i not in dtsj_old_bxhm:
+    #             dtsj_old_bxhm.append(item_old_bxhm.bxhm)
     return render(request, "consult.html", {
         "xm": vxm,
-        "xzbxhm": xzbxhm,
-        "xzsfhm": xzsfhm,
+        "xs_name": xs_name,
+        "xs_bxhm": xs_bxhm,
+        "xs_sfhm": xs_sfhm,
         "dtsj_old_sxjg": dtsj_old_sxjg,
-        "name_xin": jbxx_xin_name,
+        "jbxx_xin_name": jbxx_xin_name,
         "grzh_xin_sxjg": grzh_xin_sxjg,
-        "name_zl": jbxx_zl_name,
+        "jbxx_zl_name": jbxx_zl_name,
         "grzh_zl_sxjg": grzh_zl_sxjg,
 
     })
